bae819e8a1649f63d8e077f1a7191739d53747b3
mariam-egm <mariam.gerges014@gmail.com>
2019-03-21 01:51:52 +0200

    sessions table

---
+++ database/migrations/2019_03_20_234342_create_sessions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->dateTime('starts_at');
+            $table->dateTime('ends_at');
+            $table->unsignedBigInteger('gym_id');
+            $table->foreign('gym_id')->references('id')->on('gyms');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+}
