2cd1f35ae675eac5951f0acc99526de2d96e0fbf
mariam-egm <mariam.gerges014@gmail.com>
2019-03-21 01:35:56 +0200

    coaches table

---
+++ database/migrations/2019_03_20_233325_create_coaches_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoachesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coaches', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->unsignedBigInteger('at_gym_id');
+            $table->foreign('at_gym_id')->references('id')->on('gyms');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coaches');
+    }
+}
