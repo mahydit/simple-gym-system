9331aee7f8edaac33bf4bdbd5b9c124c2635b667
mariam-egm <mariam.gerges014@gmail.com>
2019-03-21 14:45:28 +0200

    countries package

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
 
+        'Webpatser\Countries\CountriesServiceProvider',
         /*
          * Application Service Providers...
          */
@@ -174,7 +175,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
     ],
 
     /*
@@ -225,7 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Countries' => 'Webpatser\Countries\CountriesFacade',
     ],
 
 ];
