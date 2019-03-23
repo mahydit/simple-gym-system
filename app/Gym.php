1887b16067bd1d8d7a1e40fdf9ac1a5bb3599a24
YousraEssam <yousraa.essam.15@hotmail.com>
2019-03-23 17:55:27 +0200

    update gym model

--- app/Gym.php
+++ app/Gym.php
@@ -9,4 +9,35 @@ class Gym extends Model
     protected $fillable = [
 
     ];
+    protected $table = 'gyms';
+    public $timestamps = false;
+
+    public function gymManagers(){
+
+        return $this->hasMany('App\GymManager');
+    }
+
+    public function coaches(){
+
+        return $this->hasMany('App\Coach');
+
+    }
+
+    public function sessions(){
+
+        return $this->hasMany('App\Session');
+
+    }
+
+    public function purchaseHistory(){
+
+        return $this->hasMany('App\Purchase');
+
+    }
+
+    public function city(){
+
+        return $this->belongsTo('App\City');
+
+    }
 }
