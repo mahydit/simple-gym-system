24f120cbe3a045016218f0dcde249e760493e289
mahydit <mahydti_anber@outlook.com>
2019-03-23 10:05:34 +0200

    creating validation for storing the session

---
+++ app/Http/Requests/Session/StoreSessionRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Session;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreSessionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:3',
+            'starts_at' => 'required|timezone',
+            'ends_at' => 'required|timezone',
+            'gym_id' => 'exists:gyms,id',
+            'session_date' => 'required|date'
+        ];
+    }
+}
