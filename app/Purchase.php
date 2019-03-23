29f42ca676f28048708037499a024da01eb77b55
mahydit <mahydti_anber@outlook.com>
2019-03-23 03:35:59 +0200

    updating Purchase model

--- app/Purchase.php
+++ app/Purchase.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Purchase extends Model
 {
     protected $fillable = [
-
+        'client_id', 'gym_id', 'name', 'price',
     ];
+    
+    protected $table = 'purchase_history';
+    public $timestamps = false;
+
+    public function setPurchaseDateAttribute($value) { 
+        $this->attributes['purchase_date'] = \Carbon\Carbon::now(); 
+    }
+
 }
