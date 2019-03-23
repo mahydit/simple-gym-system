539f5c4532ae1f9000734fa565b7351e10dac0f3
mahydit <mahydti_anber@outlook.com>
2019-03-23 03:37:34 +0200

    updating Attendee model

--- app/Attendee.php
+++ app/Attendee.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Attendee extends Model
 {
     protected $fillable = [
-
+        'user_id', 'birth_date', 'gender','remaing_sessions',	
     ];
+
+    protected $table = 'attendees_users';
+    public $timestamps = false;
+    
+    public function user()
+    {
+        return $this->morphOne('App\User', 'role');
+    }
 }
