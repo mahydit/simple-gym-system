9f80d187677b0228c541c320cff1bebc619fa3e9
YousraEssam <yousraa.essam.15@hotmail.com>
2019-03-23 17:54:53 +0200

    update city model

--- app/City.php
+++ app/City.php
@@ -9,5 +9,24 @@ class City extends Model
     protected $fillable = [
 
     ];
-    
+    protected $table = 'cities';
+    public $timestamps = false;
+
+    public function gym(){
+
+        return $this->hasMany('App\Gym');
+
+    }
+
+    public function cityManager(){
+
+        return $this->hasOne('App\CityManager');
+
+    }
+
+    public function country(){
+
+        return $this->belongsTo('App\Country');
+
+    }
 }
