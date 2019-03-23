f83c001184240d64724256d8b4b4abf244041bd4
mahydit <mahydti_anber@outlook.com>
2019-03-23 03:35:23 +0200

    updating Coach Model

--- app/Coach.php
+++ app/Coach.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Coach extends Model
 {
     protected $fillable = [
-
+        'name',
     ];
+
+    public function session()
+    {
+        return $this->belongsToMany('App\Session');
+    }
 }
