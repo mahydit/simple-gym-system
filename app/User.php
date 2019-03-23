be21735f8fe56c7637a50fb85d879e2e6616dc12
YousraEssam <yousraa.essam.15@hotmail.com>
2019-03-23 17:56:15 +0200

    update user model

--- app/User.php
+++ app/User.php
@@ -64,4 +64,10 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function sessionAttendance(){
+
+        return $this->belongsTo('App\User');
+
+    }
 }
