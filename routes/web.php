53869080193b08c0ea8e17c69cf12d9605024775
YousraEssam <yousraa.essam.15@hotmail.com>
2019-03-25 18:06:19 +0200

    prep of controllers and views

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,39 @@ Route::group(['middleware' => 'auth'], function () {
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+///// CITIES //////
+Route::get      ('/cities',               'CityController@index')   ->name('cities.index');
+Route::get      ('/cities/create',        'CityController@create')  ->name('cities.create');
+Route::post     ('/cities',               'CityController@store')   ->name('cities.store');
+Route::get      ('/cities/{city}',        'CityController@show')    ->name('cities.show');
+Route::get      ('/cities/{city}/edit',   'CityController@edit')    ->name('cities.edit');
+Route::put      ('/cities/{city}',        'CityController@update')  ->name('cities.update');
+Route::delete   ('/cities/{city}/destroy','CityController@destroy') ->name('cities.destroy');
+
+///// GYMS //////
+Route::get      ('/gyms',              'GymController@index')   ->name('gyms.index');
+Route::get      ('/gyms/create',       'GymController@create')  ->name('gyms.create');
+Route::post     ('/gyms',              'GymController@store')   ->name('gyms.store');
+Route::get      ('/gyms/{gym}',        'GymController@show')    ->name('gyms.show');
+Route::get      ('/gyms/{gym}/edit',   'GymController@edit')    ->name('gyms.edit');
+Route::put      ('/gyms/{gym}',        'GymController@update')  ->name('gyms.update');
+Route::delete   ('/gyms/{gym}/destroy','GymController@destroy') ->name('gyms.destroy');
+
+///// CITY MANAGERS //////
+Route::get      ('/cityManagers',                      'CityManagerController@index')   ->name('cityManagers.index');
+Route::get      ('/cityManagers/create',               'CityManagerController@create')  ->name('cityManagers.create');
+Route::post     ('/cityManagers',                      'CityManagerController@store')   ->name('cityManagers.store');
+Route::get      ('/cityManagers/{citymanager}',        'CityManagerController@show')    ->name('cityManagers.show');
+Route::get      ('/cityManagers/{citymanager}/edit',   'CityManagerController@edit')    ->name('cityManagers.edit');
+Route::put      ('/cityManagers/{citymanager}',        'CityManagerController@update')  ->name('cityManagers.update');
+Route::delete   ('/cityManagers/{citymanager}/destroy','CityManagerController@destroy') ->name('cityManagers.destroy');
+
+///// GYM MANAGERS //////
+Route::get      ('/gymManagers',                     'CityManagerController@index')   ->name('cityManagers.index');
+Route::get      ('/gymManagers/create',              'CityManagerController@create')  ->name('cityManagers.create');
+Route::post     ('/gymManagers',                     'CityManagerController@store')   ->name('cityManagers.store');
+Route::get      ('/gymManagers/{gymmanager}',        'CityManagerController@show')    ->name('cityManagers.show');
+Route::get      ('/gymManagers/{gymmanager}/edit',   'CityManagerController@edit')    ->name('cityManagers.edit');
+Route::put      ('/gymManagers/{gymmanager}',        'CityManagerController@update')  ->name('cityManagers.update');
+Route::delete   ('/gymManagers/{gymmanager}/destroy','CityManagerController@destroy') ->name('cityManagers.destroy');
