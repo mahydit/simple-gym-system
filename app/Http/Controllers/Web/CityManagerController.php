d0df7dcf9123f29f737dda6405a1e3cb05a70c69
YousraEssam <yousraa.essam.15@hotmail.com>
2019-03-25 21:45:57 +0200

    edit controllers

---
+++ app/Http/Controllers/Web/CityManagerController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CityManagerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('cityManagers.index',[
+            'cityManagers' => City::all(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('cityManagers.create',[
+            'cityManagers' => City::all(),
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CityManager $cityManager)
+    {
+        return view('cityManager.show',[
+            'cityManager' => $cityManager
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CityManager $cityManager)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CityManager $cityManager)
+    {
+        $cityManager->delete();
+        return redirect()->route('cityManager.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -41,37 +41,37 @@ Auth::routes(['verify' => true]);
 Route::get('/home', 'HomeController@index')->name('home');
 
 ///// CITIES //////
-Route::get      ('/cities',               'CityController@index')   ->name('cities.index');
-Route::get      ('/cities/create',        'CityController@create')  ->name('cities.create');
-Route::post     ('/cities',               'CityController@store')   ->name('cities.store');
-Route::get      ('/cities/{city}',        'CityController@show')    ->name('cities.show');
-Route::get      ('/cities/{city}/edit',   'CityController@edit')    ->name('cities.edit');
-Route::put      ('/cities/{city}',        'CityController@update')  ->name('cities.update');
-Route::delete   ('/cities/{city}/destroy','CityController@destroy') ->name('cities.destroy');
+Route::get      ('/cities',               'Web\CityController@index')   ->name('cities.index');
+Route::get      ('/cities/create',        'Web\CityController@create')  ->name('cities.create');
+Route::post     ('/cities',               'Web\CityController@store')   ->name('cities.store');
+Route::get      ('/cities/{city}',        'Web\CityController@show')    ->name('cities.show');
+Route::get      ('/cities/{city}/edit',   'Web\CityController@edit')    ->name('cities.edit');
+Route::put      ('/cities/{city}',        'Web\CityController@update')  ->name('cities.update');
+Route::delete   ('/cities/{city}/destroy','Web\CityController@destroy') ->name('cities.destroy');
 
 ///// GYMS //////
-Route::get      ('/gyms',              'GymController@index')   ->name('gyms.index');
-Route::get      ('/gyms/create',       'GymController@create')  ->name('gyms.create');
-Route::post     ('/gyms',              'GymController@store')   ->name('gyms.store');
-Route::get      ('/gyms/{gym}',        'GymController@show')    ->name('gyms.show');
-Route::get      ('/gyms/{gym}/edit',   'GymController@edit')    ->name('gyms.edit');
-Route::put      ('/gyms/{gym}',        'GymController@update')  ->name('gyms.update');
-Route::delete   ('/gyms/{gym}/destroy','GymController@destroy') ->name('gyms.destroy');
+Route::get      ('/gyms',              'Web\GymController@index')   ->name('gyms.index');
+Route::get      ('/gyms/create',       'Web\GymController@create')  ->name('gyms.create');
+Route::post     ('/gyms',              'Web\GymController@store')   ->name('gyms.store');
+Route::get      ('/gyms/{gym}',        'Web\GymController@show')    ->name('gyms.show');
+Route::get      ('/gyms/{gym}/edit',   'Web\GymController@edit')    ->name('gyms.edit');
+Route::put      ('/gyms/{gym}',        'Web\GymController@update')  ->name('gyms.update');
+Route::delete   ('/gyms/{gym}/destroy','Web\GymController@destroy') ->name('gyms.destroy');
 
 ///// CITY MANAGERS //////
-Route::get      ('/cityManagers',                      'CityManagerController@index')   ->name('cityManagers.index');
-Route::get      ('/cityManagers/create',               'CityManagerController@create')  ->name('cityManagers.create');
-Route::post     ('/cityManagers',                      'CityManagerController@store')   ->name('cityManagers.store');
-Route::get      ('/cityManagers/{citymanager}',        'CityManagerController@show')    ->name('cityManagers.show');
-Route::get      ('/cityManagers/{citymanager}/edit',   'CityManagerController@edit')    ->name('cityManagers.edit');
-Route::put      ('/cityManagers/{citymanager}',        'CityManagerController@update')  ->name('cityManagers.update');
-Route::delete   ('/cityManagers/{citymanager}/destroy','CityManagerController@destroy') ->name('cityManagers.destroy');
+Route::get      ('/cityManagers',                      'Web\CityManagerController@index')   ->name('cityManagers.index');
+Route::get      ('/cityManagers/create',               'Web\CityManagerController@create')  ->name('cityManagers.create');
+Route::post     ('/cityManagers',                      'Web\CityManagerController@store')   ->name('cityManagers.store');
+Route::get      ('/cityManagers/{citymanager}',        'Web\CityManagerController@show')    ->name('cityManagers.show');
+Route::get      ('/cityManagers/{citymanager}/edit',   'Web\CityManagerController@edit')    ->name('cityManagers.edit');
+Route::put      ('/cityManagers/{citymanager}',        'Web\CityManagerController@update')  ->name('cityManagers.update');
+Route::delete   ('/cityManagers/{citymanager}/destroy','Web\CityManagerController@destroy') ->name('cityManagers.destroy');
 
 ///// GYM MANAGERS //////
-Route::get      ('/gymManagers',                     'CityManagerController@index')   ->name('cityManagers.index');
-Route::get      ('/gymManagers/create',              'CityManagerController@create')  ->name('cityManagers.create');
-Route::post     ('/gymManagers',                     'CityManagerController@store')   ->name('cityManagers.store');
-Route::get      ('/gymManagers/{gymmanager}',        'CityManagerController@show')    ->name('cityManagers.show');
-Route::get      ('/gymManagers/{gymmanager}/edit',   'CityManagerController@edit')    ->name('cityManagers.edit');
-Route::put      ('/gymManagers/{gymmanager}',        'CityManagerController@update')  ->name('cityManagers.update');
-Route::delete   ('/gymManagers/{gymmanager}/destroy','CityManagerController@destroy') ->name('cityManagers.destroy');
+Route::get      ('/gymManagers',                     'Web\GymManagerController@index')   ->name('gymManagers.index');
+Route::get      ('/gymManagers/create',              'Web\GymManagerController@create')  ->name('gymManagers.create');
+Route::post     ('/gymManagers',                     'Web\GymManagerController@store')   ->name('gymManagers.store');
+Route::get      ('/gymManagers/{gymmanager}',        'Web\GymManagerController@show')    ->name('gymManagers.show');
+Route::get      ('/gymManagers/{gymmanager}/edit',   'Web\GymManagerController@edit')    ->name('gymManagers.edit');
+Route::put      ('/gymManagers/{gymmanager}',        'Web\GymManagerController@update')  ->name('gymManagers.update');
+Route::delete   ('/gymManagers/{gymmanager}/destroy','Web\GymManagerController@destroy') ->name('gymManagers.destroy');
