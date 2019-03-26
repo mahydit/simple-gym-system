8974bfcd9ac12e1b4726923e5e598d956795335c
mahydit <mahydit_anber@outlook.com>
2019-03-26 23:27:35 +0200

    adding overlapping session validation rule

--- app/Http/Requests/Session/StoreSessionRequest.php
+++ app/Http/Requests/Session/StoreSessionRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Session;
 
+use App\Rules\Overlapping;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreSessionRequest extends FormRequest
@@ -23,10 +24,15 @@ class StoreSessionRequest extends FormRequest
      */
     public function rules()
     {
+        $starts_at=$this->starts_at;
+        $ends_at=$this->ends_at;
+        $date=$this->session_date;
+        var_dump($starts_at,$ends_at,$date,$starts_at,$ends_at,$date);
+
         return [
             'name' => 'required|min:3',
             'starts_at' => 'required|', //TODO: validate time format
-            'ends_at' => 'required|different:starts_at',
+            'ends_at' => ['required','different:starts_at',new Overlapping($starts_at,$ends_at,$date)],
             'gym_id' => 'required|exists:gyms,id',
             'session_date' => 'required|date_format:Y-m-d',
             'coach_id'=>'required|exists:coaches,id',
