490e17c7413e9e082117bf2d645687892bc4f9ef
mahydit <mahydit_anber@outlook.com>
2019-03-27 00:51:56 +0200

    using dashboard section

--- resources/views/sessions/index.blade.php
+++ resources/views/sessions/index.blade.php
@@ -1,25 +1,23 @@
 @extends('layouts.dashboard')
 
+@section('meta')
+<meta name="csrf-token" content="{{ csrf_token() }}">
+@endsection
+
+@section('style')
+<link rel="stylesheet" href="//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+@endsection
+
 @section('content')
-<!DOCTYPE html>
-
-<html lang="en">
-
-    <head>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
-    </head>
-<body>
-<section class="content">
 <div class="row">
     <div class="col-xs-12">
         <div class="box box-primary">
             <div class="box-header">
-                <p>Sessions </p>
+                <p class="box-title">Sessions </p>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
-                <table id="session_table" class="table text-center">
+                <table id="session_table" class="table table-bordered table-hover">
                     <thead>
                         <tr>
                             <th class="text-center">#</th>
@@ -40,86 +38,120 @@
     </div>
 </div>
 
-<div class="modal fade" id="deletepopup" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h3 class="modal-title" id="exampleModalLabel">Are you sure you want to delete this Package</h3>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+<div class="modal fade" id="deletepopup" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title" id="exampleModalLabel">Are you sure you want to delete this Package</h3>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-footer">
+                <div>
+                    <div id="csrf_value" hidden>@csrf</div>
+                    {{ method_field('DELETE') }}
+                    <button type="button" row_delete="" id="delete_item" class="btn btn-danger"
+                        data-dismiss="modal">Yes</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">NO</button>
                 </div>
-                <div class="modal-footer">
-                    <div>
-                        <div id="csrf_value"  hidden >@csrf</div>
-                            {{ method_field('DELETE') }}
-                            <button type="button" row_delete="" id="delete_item"  class="btn btn-danger" data-dismiss="modal">Yes</button>
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">NO</button>
-                    </div>
 
-                </div>
             </div>
         </div>
     </div>
-    </section>
+</div>
+@endsection
 
-    <script src="//code.jquery.com/jquery.js"></script>
-        <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+@section('plugins')
+<script src="//code.jquery.com/jquery.js"></script>
+<script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+@endsection
 
-        <script>
+@section('script')
+<script>
+    $(function () {
+        $('#session_table').DataTable({
+            processing: true,
+            serverSide: true,
+            'paging': true,
+            'searching': true,
+            'ordering': true,
+            'info': true,
+            'autoWidth': true,
+            "bLengthChange": true,
+            'autoWidth': true,
 
-$(function() {
-            $('#session_table').DataTable({
-                processing: true,
-                serverSide: true,
-                'paging'      : true,
-                'searching'   : true,
-                'ordering'    : true,
-                'info'        : true,
-                'autoWidth'   : true,
-                "bLengthChange": true,
-                'autoWidth'   : true,
-
-                ajax: '{!! route('get.session') !!}',
-                columns: [
-                    { data: 'id', name: 'id' },
-                    { data: 'name', name: 'name' },
-                    { data: 'gym.name', name: 'gym.name' },
-                    { data: 'coaches[].name', name: 'coaches[].name' },
-                    { data: 'starts_at', name: 'starts_at' },
-                    { data: 'ends_at', name: 'ends_at' },
-                    { data: 'session_date', name: 'session_date' },
-                    
-/* Show */ {
-    mRender: function (data, type, row) {
-                        return '<center><a href="/sessions/'+row.id+'" class="table-delete btn btn-info" data-id="' + row.id + '">Show</a></center>'
-                    }
+            ajax: '{!! route('
+            get.session ') !!}',
+            columns: [{
+                    data: 'id',
+                    name: 'id'
                 },
-                /* EDIT */ {
+                {
+                    data: 'name',
+                    name: 'name'
+                },
+                {
+                    data: 'gym.name',
+                    name: 'gym.name'
+                },
+                {
+                    data: 'coaches[].name',
+                    name: 'coaches[].name'
+                },
+                {
+                    data: 'starts_at',
+                    name: 'starts_at'
+                },
+                {
+                    data: 'ends_at',
+                    name: 'ends_at'
+                },
+                {
+                    data: 'session_date',
+                    name: 'session_date'
+                },
+
+                /* Show */
+                {
                     mRender: function (data, type, row) {
-                        return '<center><a href="/sessions/'+row.id+'/edit" class="table-edit btn btn-warning" data-id="' + row.id + '">Edit</a></center>'
+                        return '<center><a href="/sessions/' + row.id +
+                            '" class="table-delete btn btn-info" data-id="' + row.id +
+                            '">Show</a></center>'
                     }
                 },
-                /* DELETE */ {
+                /* EDIT */
+                {
                     mRender: function (data, type, row) {
-                        return '<center><a href="#" class="table-delete btn btn-danger" row_id="' + row.id + '" data-toggle="modal" data-target="#deletepopup" id="delete_toggle">Delete</a></center>'
+                        return '<center><a href="/sessions/' + row.id +
+                            '/edit" class="table-edit btn btn-warning" data-id="' + row.id +
+                            '">Edit</a></center>'
                     }
                 },
-            
+                /* DELETE */
+                {
+                    mRender: function (data, type, row) {
+                        return '<center><a href="#" class="table-delete btn btn-danger" row_id="' +
+                            row.id +
+                            '" data-toggle="modal" data-target="#deletepopup" id="delete_toggle">Delete</a></center>'
+                    }
+                },
+
             ],
         });
-        $(document).on('click','#delete_toggle',function () {
+        $(document).on('click', '#delete_toggle', function () {
             var delete_id = $(this).attr('row_id');
-            $('#delete_item').attr('row_delete',delete_id);
+            $('#delete_item').attr('row_delete', delete_id);
         });
-        
-        $(document).on('click','#delete_item',function () {
+
+        $(document).on('click', '#delete_item', function () {
             var session_id = $(this).attr('row_delete');
             $.ajax({
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 },
-                url: '/sessions/'+session_id,
+                url: '/sessions/' + session_id,
                 type: 'DELETE',
                 success: function (data) {
                     console.log('success');
@@ -133,13 +165,8 @@ $(function() {
                 }
             });
         });
-       
+
     });
-    </script>
 
-@stack('scripts')
-
-</body>
-
-</html>
+</script>
 @endsection
