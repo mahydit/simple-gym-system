225af8a0523f8107fa28a52a53bcfc2a709c75cf
Shaf3y <Shaf3iibrahim@gmail.com>
2019-03-27 18:10:12 +0200

    make NotifyUser Command

---
+++ app/Console/Commands/NotifyUser.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class NotifyUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'notify:users-not-logged-in-for-month';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "notify users that didn't log in for a month";
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        
+    }
+}
