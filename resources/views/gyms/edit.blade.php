15f2b259d791cb8b49d376c460dea996662db361
YousraEssam <yousraa.essam.15@hotmail.com>
2019-03-27 23:03:47 +0200

    edit blade in gym

--- resources/views/gyms/edit.blade.php
+++ resources/views/gyms/edit.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
 <a href="{{route('gyms.index')}}" class="btn btn-danger">Back</a>
 
 <form action="{{route('gyms.update',[$gym->id])}}" method="POST">
@@ -23,16 +23,16 @@
     </div>
     <div class="form-group">
         <label for="exampleInputPassword1">City Name</label>
-        @foreach($cities as $city)
-        <textarea name="name" class="form-control" value="{{$gym->city_id}}">{{$city->name}}</textarea>
-
-        <!-- <textarea name="name" class="form-control" value="{{$gym->city_id}}">{{$gym->city_id['name']}}</textarea> -->
-        @endforeach
+        <select class="form-control" name="city_id">
+            @foreach($cities as $city)
+            <option value="{{$city->id}}">{{$city->name}}</option>
+            @endforeach
+        </select>
     </div>
 
     <div class="form-group">
-            <label for="exampleInputPassword1">Image</label>
-            <button type="button" class="btn btn-light">Upload Image</button>
+        <label for="exampleInputPassword1">Image</label>
+        <button type="button" class="btn btn-light">Upload Image</button>
     </div>
 
     <button type="submit" class="btn btn-primary">Submit</button>
@@ -40,6 +40,6 @@
 
 @endsection
 @section('plugins')
- <!-- jQuery 3 -->
- <script src="{{ asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
+<!-- jQuery 3 -->
+<script src="{{ asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
 @endsection
