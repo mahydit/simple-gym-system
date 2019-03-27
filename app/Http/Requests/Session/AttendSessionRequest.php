ea5cff292025cf8b79f3a26a508373f60589b47e
Shaf3y <Shaf3iibrahim@gmail.com>
2019-03-27 14:00:33 +0200

    remaining sessions custom validation error

--- app/Http/Requests/Session/AttendSessionRequest.php
+++ app/Http/Requests/Session/AttendSessionRequest.php
@@ -4,6 +4,8 @@ namespace App\Http\Requests\Session;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class AttendSessionRequest extends FormRequest
 {
@@ -24,9 +26,9 @@ class AttendSessionRequest extends FormRequest
      */
     public function rules()
     {
-
         return [
             "session_date"  => "date_format:Y-m-d|date_equals:today",
+            "remain_sessions" => "gt:0",
         ];
     }
 
@@ -34,6 +36,15 @@ class AttendSessionRequest extends FormRequest
     {
         $this->merge([
             'session_date' => $this->route('session')->session_date,
+            'remain_sessions' => Auth::user()->role->remain_sessions,
         ]);
+    }
+
+    public function messages(){
+
+        return [
+
+            'remain_sessions.gt' => "You Have No Remaining Sessions Left Please Buy A Session in Order To Attend",
+        ];
     }
 }
