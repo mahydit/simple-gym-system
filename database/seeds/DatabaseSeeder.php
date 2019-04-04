f47fca0c19c10810b52c106431551db75c2aa9ed
Mahydit Mahmoud Anber <Mahydit_anber@outlook.com>
2019-04-04 22:43:46 +0200

    add RolesAndPermissions to DatabaseSeeder class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
         $this->call('CountriesSeeder');
         $this->command->info('Seeded the countries!');
+
+        $this->call('RolesAndPermissionsSeeder');
+        $this->command->info('Seeded Roles and Permissions!');
     }
 }
